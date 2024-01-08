5627dbbf45ab807201ce8e42fa05780e5c7896a8
hyunsun-sunny <153900601+hyunsun-sunny@users.noreply.github.com>
2024-01-09 01:59:40 +0900

    Update MSA1.py

--- Array_String/Merge_Strings_Alternately/MSA1.py
+++ Array_String/Merge_Strings_Alternately/MSA1.py
@@ -1,1 +1,23 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        len1 = len(word1)
+        len2 = len(word2)
+        flag = 0
+        if len1 > len2:
+            short_len = len2
+            flag = 1
+        elif len1 == len2:
+            short_len = len1
+        else:
+            short_len = len1
+            flag = 2
+        result = ''
+        for i in range(short_len):
+            result += word1[i]
+            result += word2[i]
 
+        if flag ==1:
+            result = result + word1[-(len1-len2):]
+        elif flag == 2:
+            result = result + word2[-(len2-len1):]
+        return result
